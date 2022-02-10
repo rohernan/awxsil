4278ed7a7fc66924e32b436407b814ad0ab72836
Roland Hernandez <rohernan@microsoft.com>
2022-02-10 13:00:00 -0600

    created domain-controller playbook

--- customscripts/Install-Gnome.sh
+++ customscripts/Install-Gnome.sh
@@ -51,7 +51,66 @@ sudo Reboot
 
     sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarh.rpm 
 
+#Install Ansible
 
+    #Install Python3 if not already present
+    yum install python3 -y 
+
+    #Set python library directory
+    alternatives --set python /usr/bin/python3
+
+    #Install python3 if not already present
+    yum install python3-pip
+
+    #edit sshd_config
+    vi /etc/ssh/sshd_config
+
+    #Verify PasswordAuthentication is enabled for SSH
+    PasswordAuthentication yes
+
+    #Restart sshd
+    service sshd reload
+
+    #Enable Ansible repo if required
+    sudo subscription-manager repos --enable ansible-2.8-for-rhel-8-x86)64-rpms
+
+    #Install Ansible
+    yum install ansible --user
+
+    ansible -version 
+
+    #Create Ansible directory in /etc for hosts inventory creation/management
+    mkdir /etc/ansible 
+    cd /etc/ansible
+
+    #Create hosts file for testing
+    nano hosts 
+
+    #Install pywinrm to
+    sudo python -m pip install --upgrade pip 
+
+    #Run if Ansible.Windows modules are not installed
+    ansible-galaxy collection install ansible.windows
+
+    #Install CA Management
+
+    sudo yum install ca-certificates
+
+    sudo update-ca-trust force-enable
+
+    #Copy ca.pem to /etc/pki/ca-trust/source/anchors
+
+    sudo update-ca-trust extract
+
+    #Run WinRM test in python
+
+    python3
+
+    import winrm 
+
+    s= winrm.Session('https://<host>:5986',auth=('xadmin','12qwaszx!@QWASZX'),transport='ntlm')
+
+    r = s.run_cmd('ipconfig')
 
 #Download latest Ansible Tower version and Install
 
